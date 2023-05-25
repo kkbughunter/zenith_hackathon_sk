a2799debd7cbc406dee5ee7830ac7f19e7526bb8
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-05-25 11:15:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -257,4 +257,21 @@ const rainChart = new Chart(document.getElementById("rain_graph"), {
   }
 });
 
-
+// /statuses/override
+const button1 = document.getElementById("mode-btn");
+const button2 = document.getElementById("onoff-btn");
+const enable = document.getElementById("enable");
+const disable = document.getElementById("disable");
+// function to disable the buttons
+const disableButtons = () => {
+  button1.disabled = true;
+  button2.disabled = true;
+};
+const enableButtons = () => {
+  button1.disabled = false;
+  button2.disabled = false;
+};
+// calling the disableButtons() function when the disable button is clicked
+disable.addEventListener("click", disableButtons);
+// calling the enableButtons() function when the enable button is clicked
+enable.addEventListener("click", enableButtons);
