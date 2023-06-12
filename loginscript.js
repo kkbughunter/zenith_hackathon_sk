e1f0b1bb531ee57539282181edfc39836292f45f
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-06-12 23:40:58 +0530

    Update loginscript.js

--- loginscript.js
+++ loginscript.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB6-6vGAQcsuqMm172-IkMomesTy_Uk0Y8",
-    authDomain: "weather-pi2app.firebaseapp.com",
-    databaseURL: "https://weather-pi2app-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "weather-pi2app",
-    storageBucket: "weather-pi2app.appspot.com",
-    messagingSenderId: "241617772757",
-    appId: "1:241617772757:web:534f21ef8fbe302639425c",
-    measurementId: "G-T7R1DMZGN1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
